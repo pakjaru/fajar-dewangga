31af0feb6903cd5fd87cc2e940d10adc793ce674
pakjaru <fajarudewangga@gmail.com>
2022-08-31 19:58:50 +0800

    remove package lock, init yarn lock

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -1,5 +1,7 @@
 import type { GatsbyConfig } from "gatsby"
 
+const path = require('path')
+
 const config: GatsbyConfig = {
   siteMetadata: {
     siteUrl: `https://www.yourdomain.tld`,
@@ -8,7 +10,27 @@ const config: GatsbyConfig = {
   // If you use VSCode you can also use the GraphQL plugin
   // Learn more at: https://gatsby.dev/graphql-typegen
   graphqlTypegen: true,
-  plugins: [],
+  plugins: [
+    `gatsby-plugin-react-helmet`,
+    {
+      resolve: `gatsby-plugin-styled-components`,
+      options: {
+        "displayName": false,
+        "fileName": false
+      },
+    },
+    {
+      resolve: `gatsby-plugin-alias-imports`,
+      options: {
+        alias: {
+          "@utils": path.resolve(__dirname, 'src/utils')
+        },
+        extensions: [
+          "ts"
+        ]
+      }
+    }
+  ],
 }
 
 export default config
