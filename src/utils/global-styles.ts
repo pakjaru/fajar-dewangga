1de5a2c412ae1e5a23135ecac2769007421629d0
pakjaru <fajarudewangga@gmail.com>
2022-09-15 21:52:55 +0800

    add color variable on global styles

--- src/utils/global-styles.ts
+++ src/utils/global-styles.ts
@@ -1,9 +1,14 @@
 import { createGlobalStyle } from "styled-components"
 
+export const background = '#0a192f'
+export const primary = '#8892b0'
+export const primaryLight = '#ccd6f6'
+export const secondary = '#64ffda'
+
 export const GlobalStyles = createGlobalStyle`
   body {
-    background-color: #0a192f;
-    color: #8892b0;
+    background-color: ${background};
+    color: ${primary};
     font-size: 16px;
     font-family: 'Roboto', sans-serif;
     margin: 0;
