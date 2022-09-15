3c174d3f0f4a3f52bf5dec608e4600724e207477
pakjaru <fajarudewangga@gmail.com>
2022-09-15 21:53:13 +0800

    implement header desktop only

--- src/components/header/header.styles.ts
+++ src/components/header/header.styles.ts
@@ -1,7 +1,12 @@
-import styled from "styled-components";
+import styled from "styled-components"
+
+import { primaryLight, secondary } from "@utils/global-styles"
 
 export const HeaderStyle = styled.header`
-
+  align-items: center;
+  display: flex;
+  font-family: 'Roboto Mono', monospace;
+  height: 100px;
 `
 
 export const HeaderContainer = styled.div`
@@ -10,24 +15,58 @@ export const HeaderContainer = styled.div`
   justify-content: space-between;
   margin: auto;
   max-width: 1820px;
+  width: 100%;
 `
 
 export const HeaderLogo = styled.a`
+  height: 52px;
 
+  img {
+    height: 100%;
+  }
 `
 
 export const Nav = styled.nav`
-
+  align-items: center;
+  display: flex;
 `
 
 export const NavWrapper = styled.ol`
-
+  align-items: center;
+  display: flex;
+  list-style: none;
+  margin: 0;
+  padding: 0;
 `
 
 export const NavItem = styled.li`
+  counter-increment: item 1;
+  margin-right: 30px;
 
+  &:last-child {
+    margin-right: 0;
+  }
+
+  a {
+    color: ${primaryLight};
+    font-size: 14px;
+    text-decoration: none;
+
+    &::before {
+      content: "0" counter(item) ".";
+      margin-right: 5px;
+      color: ${secondary};
+      font-size: 14px;
+  }
 `
 
 export const NavButton = styled.button`
-
+  background-color: transparent;
+  border: 1px solid ${secondary};
+  border-radius: 4px;
+  color: ${secondary};
+  cursor: pointer;
+  font-size: 14px;
+  margin-left: 30px;
+  padding: 10px 20px;
 ` 
--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,9 +1,35 @@
 import React, { FunctionComponent } from "react"
 
+import { logo } from "@images"
+
 import { HeaderProps } from "./header.props"
+import {
+  HeaderContainer,
+  HeaderLogo,
+  HeaderStyle,
+  Nav,
+  NavButton,
+  NavItem,
+  NavWrapper 
+} from "./header.styles"
 
 export const Header: FunctionComponent<HeaderProps> = () => {
   return (
-    <p>header</p>
+    <HeaderStyle>
+      <HeaderContainer>
+        <HeaderLogo href="/" className="header__logo">
+          <img src={logo} alt="Logo" />
+        </HeaderLogo>
+
+        <Nav>
+          <NavWrapper>
+            <NavItem><a href="/#about">About</a></NavItem>
+            <NavItem><a href="/#work">Work</a></NavItem>
+            <NavItem><a href="/#contact">Contact</a></NavItem>
+          </NavWrapper>
+          <NavButton>Resume</NavButton>
+        </Nav>
+      </HeaderContainer>
+    </HeaderStyle>
   )
 }
