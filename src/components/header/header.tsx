fc62cd624654442038fb6a5364dcce50abc8a259
pakjaru <fajarudewangga@gmail.com>
2022-09-27 22:08:22 +0800

    create header component

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,4 +1,4 @@
-import React, { FunctionComponent } from "react"
+import React, { FunctionComponent, RefObject, useEffect, useRef } from "react"
 
 import { logo } from "@images"
 
@@ -10,18 +10,49 @@ import {
   Nav,
   NavButton,
   NavItem,
+  NavMenu,
   NavWrapper 
 } from "./header.styles"
 
 export const Header: FunctionComponent<HeaderProps> = () => {
+  const menuRef = useRef<HTMLElement>()
+  const navRef = useRef<HTMLButtonElement>()
+
+  const onMenuClickHandler = () => {
+    if (!menuRef?.current || !navRef?.current) return
+
+    menuRef.current.classList.toggle('active')
+    navRef.current.classList.toggle('active')
+  }
+
+  const onWindowClickHandler = ({ target }: MouseEvent) => {
+    if (window.innerWidth > 767 || !menuRef?.current || !navRef?.current) return
+    
+    if (
+      !menuRef.current.classList.contains('active') &&
+      !navRef.current.classList.contains('active')
+    ) return
+
+    if (menuRef.current.contains(target as Node)) return
+
+    menuRef.current.classList.remove('active')
+    navRef.current.classList.remove('active')
+  }
+
+  useEffect(() => {
+    window.addEventListener('click', (e) => onWindowClickHandler(e))
+
+    return () => window.removeEventListener('click', (e) => onWindowClickHandler(e))
+  }, [])
+
   return (
     <HeaderStyle>
       <HeaderContainer>
-        <HeaderLogo href="/" className="header__logo">
+        <HeaderLogo href="/">
           <img src={logo} alt="Logo" />
         </HeaderLogo>
 
-        <Nav>
+        <Nav ref={navRef as RefObject<HTMLElement>}>
           <NavWrapper>
             <NavItem><a href="/#about">About</a></NavItem>
             <NavItem><a href="/#work">Work</a></NavItem>
@@ -29,6 +60,11 @@ export const Header: FunctionComponent<HeaderProps> = () => {
           </NavWrapper>
           <NavButton>Resume</NavButton>
         </Nav>
+
+        <NavMenu ref={menuRef as RefObject<HTMLButtonElement>} onClick={onMenuClickHandler}>
+          <span></span>
+          <span></span>
+        </NavMenu>
       </HeaderContainer>
     </HeaderStyle>
   )
