f69f79959080603c1acf73b6e68633d1fb2ec5b1
pakjaru <fajarudewangga@gmail.com>
2022-10-25 21:23:13 +0800

    create footer component

---
+++ src/components/footer/footer.styles.ts
@@ -0,0 +1,32 @@
+import styled from "styled-components"
+
+import { primary, secondary } from "@utils/global-styles"
+
+export const FooterMain = styled.footer`
+  margin-top: 100px;
+  padding: 50px 0;
+`
+
+export const FooterWrapper = styled.div`
+  align-items: center;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+`
+
+export const FooterText = styled.a`
+  color: ${primary};
+  font-family: 'Roboto Mono', monospace;
+  font-size: 12px;
+  margin: 10px 0 0 0;
+  text-decoration: none;
+  transition: color 0.2s ease-in-out;
+
+  &:first-child {
+    margin-top: 0;
+  }
+
+  &:hover {
+    color: ${secondary};
+  }
+`
