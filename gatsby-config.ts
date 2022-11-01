150353f443f4bcb0f75dce533f86a3c28dc16311
pakjaru <fajarudewangga@gmail.com>
2022-11-02 00:28:32 +0800

    add favicon

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -4,7 +4,7 @@ const path = require('path')
 
 const config: GatsbyConfig = {
   siteMetadata: {
-    siteUrl: `https://www.yourdomain.tld`,
+    siteUrl: `https://fajardewangga.gtsb.io/`,
   },
   // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
   // If you use VSCode you can also use the GraphQL plugin
@@ -12,6 +12,12 @@ const config: GatsbyConfig = {
   graphqlTypegen: true,
   plugins: [
     `gatsby-plugin-react-helmet`,
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        icon: "src/images/favicon.png"
+      }
+    },
     {
       resolve: `gatsby-plugin-styled-components`,
       options: {
