0020c3934a41ac58a1b28d563843208d4e57c69a
pakjaru <fajarudewangga@gmail.com>
2022-11-02 00:09:35 +0800

    improve footer

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -1,10 +1,12 @@
 import React, { FunctionComponent } from "react"
 
+import { github, instagram, linkedin } from "@images"
 import { Container } from "@utils/global-styles"
 
 import { FooterProps } from "./footer.props"
 import {
   FooterMain,
+  FooterMenu,
   FooterText,
   FooterWrapper
 } from "./footer.styles"
@@ -13,6 +15,18 @@ export const Footer: FunctionComponent<FooterProps> = () => {
   return (
     <FooterMain>
       <Container>
+        <FooterMenu>
+          <a href="https://github.com/pakjaru" target="_blank">
+            <img src={github} alt="GitHub" />
+          </a>
+          <a href="https://www.linkedin.com/in/fajar-dewangga-ba939a183" target="_blank">
+            <img src={linkedin} alt="LinkedIn" />
+          </a>
+          <a href="https://www.instagram.com/pakjaru" target="_blank">
+            <img src={instagram} alt="Instagram" />
+          </a>
+        </FooterMenu>
+
         <FooterWrapper>
           <FooterText href="https://github.com/bchiang7" target="_blank" rel="noopener noreferrer">Designed by Brittany Chiang</FooterText>
           <FooterText href="https://github.com/pakjaru" target="_blank" rel="noopener noreferrer">Built by Fajar Dewangga</FooterText>
