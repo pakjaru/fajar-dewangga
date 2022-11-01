e707292bfc784bb237a0c7426dbeeca812d2c6cf
pakjaru <fajarudewangga@gmail.com>
2022-11-01 21:54:05 +0800

    improve scroll to section utils

--- src/utils/scroll-to-section.ts
+++ src/utils/scroll-to-section.ts
@@ -1,6 +1,7 @@
 export const scrollToSection = (section: string) => {
+  const offsetConstant = window.innerWidth < 575 ? 50 : 100 
   const elementOffset = document.querySelector<HTMLElement>(`#${section}`)?.offsetTop
-  const offset = elementOffset ? elementOffset - 100 : 0
+  const offset = elementOffset ? elementOffset - offsetConstant : 0
 
   window.scrollTo({ top: offset, behavior: 'smooth' })
 }
